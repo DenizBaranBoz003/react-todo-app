9881f562cbd079dd8d35ed6b85c5453acb50c5fd
deniz <denizbaran.boz@gmail.com>
2026-02-13 18:27:02 +0300

    fix: case sensitive import path

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
-import TodoItem from "../components/TodoItem";
+import TodoItem from "../Components/TodoItem";
+
 
 function Home() {
   const [task, setTask] = useState("");
